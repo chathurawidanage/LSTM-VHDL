576995324654eadf537145de95540197710b68bb
Chathura Widanage <chathurawidanage@gmail.com>
2020-03-10 22:46:11 -0400

    initial commit

---
+++ final_project.srcs/sources_1/new/float_adder.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/10/2020 10:33:13 PM
+-- Design Name: 
+-- Module Name: float_adder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity float_adder is
+--  Port ( );
+end float_adder;
+
+architecture Behavioral of float_adder is
+
+begin
+
+
+end Behavioral;
