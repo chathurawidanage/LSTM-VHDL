d74d751f3e9656ba1c13871d3f18836204dd02ab
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2020-03-18 19:32:36 -0400

    Adde with tb

--- final_project.srcs/sources_1/new/float_adder.vhd
+++ final_project.srcs/sources_1/new/float_adder.vhd
@@ -1,43 +1,110 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/10/2020 10:33:13 PM
--- Design Name: 
--- Module Name: float_adder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity float_adder is
---  Port ( );
+    port (
+    clk: in std_logic;
+    f1: in std_logic_vector(31 downto 0);
+    f2: in std_logic_vector(31 downto 0);
+    out1: out std_logic_vector(31 downto 0)
+    );
 end float_adder;
 
-architecture Behavioral of float_adder is
+architecture float_adder of float_adder is
 
 begin
+    process(clk) is
+    variable e1: unsigned(7 downto 0) := (others => '0');
+    variable e2: unsigned(7 downto 0) := (others => '0');
+    variable newe: unsigned(7 downto 0) := (others => '0');
+    variable m1: std_logic_vector(22 downto 0) := (others => '0');
+    variable m2: std_logic_vector(22 downto 0) := (others => '0');
+    
+    variable newm1: unsigned(24 downto 0) := (others => '0');
+    variable newm2: unsigned(24 downto 0) := (others => '0');
+    variable addm: std_logic_vector(24 downto 0) := (others => '0');
+    variable newm: std_logic_vector(22 downto 0) := (others => '0');
 
+    variable dif: integer := 0;
+    
 
-end Behavioral;
+    variable over: integer := 1;
+    variable shift: integer := 0;
+    variable mval: std_logic_vector(24 downto 0) := (others => '0');
+    
+    variable sign: std_logic := '0';
+    begin
+        if rising_edge(clk) then
+             e1 := unsigned(f1(30 downto 23));
+             e2 := unsigned(f2(30 downto 23));
+             m1 := f1(22 downto 0);
+             m2 := f2(22 downto 0);
+             
+             if e1 < e2 then
+                 dif := to_integer(e2 - e1);
+                
+                 newm1 := shift_right(unsigned("01" & m1), dif);
+                 newm2 := unsigned("01" & m2);
+                 
+                 newe := e2;
+             else
+                 dif := to_integer(e1 - e2);
+                 
+                 newm1 := unsigned("01" & m1);
+                 newm2 := shift_right(unsigned("01" & m2), dif);
+                 
+                 newe := e1;
+             end if;
+             
+             if f1(31) = '1' then
+                if f2(31) = '1' then
+                    sign := '1';
+                    addm := std_logic_vector(newm1 + newm2);
+                else
+                    if newm1 < newm2 then
+                        sign := '0';
+                        addm := std_logic_vector(newm2 - newm1);    
+                    else
+                        sign := '1';
+                        addm := std_logic_vector(newm1 - newm2);
+                    end if;
+                end if;
+             else
+                if f2(31) = '1' then
+                    if newm1 < newm2 then
+                        sign := '1';
+                        addm := std_logic_vector(newm2 - newm1);
+                    else
+                        sign := '0';
+                        addm := std_logic_vector(newm1 - newm2);
+                    end if;
+                else
+                    sign := '0';
+                    addm := std_logic_vector(newm1 + newm2);
+                end if;
+             end if;
+                       
+             over := 1;
+             shift := 0;
+             for i in 24 downto 0 loop
+                --report "Logic " & std_logic'image(m(i));
+                 if addm(i) = '1' then
+                     exit;
+                 end if; 
+                 over := over - 1;
+                 shift := shift + 1;   
+             end loop;
+            
+             addm := std_logic_vector(shift_left(unsigned(addm), shift));
+            
+             newm := addm(23 downto 1);
+             newe := newe + over; 
+             
+             out1 <= sign & std_logic_vector(newe) & newm;
+        end if;
+    end process;
+
+end float_adder;
